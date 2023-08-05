80ba670ea2b3403136cb333718eb87488ef8501b
lihao <lihao@163.com>
2023-08-05 21:06:03 +0800

    L2 do it

---
+++ goodbye.c
@@ -0,0 +1,1 @@
+#define MONDAY 1
--- hello.c
+++ hello.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 int main(){
 	printf("hello, world\n");
+	printf("hello, mother!\n");
+	printf("hello, brother\n");
+	printf("hello, sister!\n");
 	return 0;
 }
