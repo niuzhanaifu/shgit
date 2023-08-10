ae61116aca50bed0484f162f5c79f24651c321a4
lihao <76874137+niuzhanaifu@users.noreply.github.com>
2023-08-10 23:40:34 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main(){
 	printf("hello, world\n");
-	printf("hello, mother!\n");
-	printf("hello, brother\n");
+	printf("hello, brother, my good boy!\n");
 	printf("hello, sister!\n");
 	return 0;
 }
