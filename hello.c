e428c70cb623187c5eff7390b5f8c7873d879bc7
lihao <lihao@163.com>
2023-08-10 23:43:12 +0800

    hhsd

--- hello.c
+++ hello.c
@@ -2,6 +2,9 @@
 int main(){
 	printf("hello, world\n");
 	printf("hello, brother, my good boy!\n");
+	printf("hello, mother!\n");
+	printf("hello, brother, better me\n");
 	printf("hello, sister!\n");
+	printf();
 	return 0;
 }
