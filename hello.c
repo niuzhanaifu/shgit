4b96dd4d74c857eaace7d30f5dea4e0680d51374
lihao <lihao@qq.com>
2023-09-03 02:29:31 -0700

    update

--- hello.c
+++ hello.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 int main(){
 	printf("hello, world\n");
-	printf("hello, brother, my good boy!\n");
 	printf("hello, mother!\n");
 	printf("hello, today!\n");
-	printf();
-	printf("hello, brother\n");
-	printf("hello, sister!\n");
 	for (int i = 0; i<10; i++) {
 	    printf("hello");
 	}
+	printf("this is a page!\n");
 	return 0;
 }
--- learn.c
+++ learn.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+
+#define MAX_NUM 10
+
 int main(){
     int sum = 0;
     for (int i=0; i < 10; ++i) {
