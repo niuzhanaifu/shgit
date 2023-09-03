7eb7e5a94d9cba67fa41d9160c3e8384c6c6abf3
lihao <76874137+niuzhanaifu@users.noreply.github.com>
2023-09-03 17:33:06 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 int main(){
-	printf("hello, world\n");
-	printf("hello, mother!\n");
+	printf("hello, world!\n");
 	printf("hello, today!\n");
 	for (int i = 0; i<10; i++) {
 	    printf("hello");
