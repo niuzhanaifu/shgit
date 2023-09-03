40fc3a59dc89949cc4930c75ef820882c97b574f
lihao <76874137+niuzhanaifu@users.noreply.github.com>
2023-09-03 17:01:57 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,8 +3,7 @@ int main(){
 	printf("hello, world\n");
 	printf("hello, brother, my good boy!\n");
 	printf("hello, mother!\n");
-	printf("hello, brother, better me\n");
-	printf("hello, sister!\n");
+	printf("hello, today!\n");
 	printf();
 	return 0;
 }
