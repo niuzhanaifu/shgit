5ade5742a9951aafff4d8d9ebe0f683a01eb552f
lihao <lihao@qq.com>
2023-09-03 02:45:19 -0700

    update

--- hello.c
+++ hello.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 int main(){
+
 	printf("hello, world!!!\n");
+	printf("hello, mother!\n");
 	printf("hello, today!\n");
 	for (int i = 0; i<10; i++) {
-	    printf("hello");
+		printf("hello");
 	}
 	return 0;
 }
